4de846c67b4d55265bd5110bf1b7ea6bf9ddc4d2
buttilloa <rufrrufr@yahoo.com>
2014-11-05 08:48:52 -0500

    Get er done

---
+++ InsectGenerator/InsectGenerator/InsectGenerator/Bug.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace InsectGenerator
+{
+    enum BugMoods
+    {
+        Timid,
+        Angry,
+        Normal
+    }
+
+
+    class Bug : Sprite
+    {
+        public BugMoods mood = BugMoods.Normal;
+
+        private Random rand = new Random((int)DateTime.UtcNow.Ticks);
+
+        float timeRemaining = 0.0f;
+        float timeTotal = 0.3f;
+        float TimePerUpdate = 2.00f;
+
+        float steerTimer = 0f;
+        float steerTime = 270f;
+
+        public Bug(
+           Vector2 location,
+           Texture2D texture,
+           Rectangle initialFrame,
+           Vector2 velocity)
+                : base(location, texture, initialFrame, velocity)
+        {
+            System.Threading.Thread.Sleep(1);
+        }
+        
+        public override void Update(GameTime gameTime)
+        {
+            if (Location.Y > 471 && velocity.Y > 0)   
+                 Velocity *= new Vector2(1, -1);
+
+            if (Location.Y < -80 && velocity.Y < 0)
+                Velocity *= new Vector2(1, -1);
+
+            steerTimer += (float)gameTime.ElapsedGameTime.Milliseconds;
+
+            if (steerTimer > steerTime)
+            {
+                steerTimer = rand.Next(0, (int)steerTime/2);
+
+                Vector2 mpos = new Vector2(Location.X + 400 * (velocity.X > 0 ? 1 : -1), Location.Y + rand.Next(-300, 300));
+
+                Vector2 vel = mpos - Location;
+                vel.Normalize();
+                vel *= 100;
+
+                Rotation = (float)Math.Atan2(vel.Y, vel.X);
+
+                Velocity = vel;
+            }
+
+            //FlipHorizontal = velocity.X < 0;
+
+
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            
+            /*if (mood == BugMoods.Angry)
+            {
+                this.TintColor = Color.Red;
+                this.Velocity *= new Vector2(1.1f, 1f);
+
+                if (Velocity.Length() > 150)
+                {
+                    this.velocity.Normalize();
+                    this.velocity *= 150;
+                }
+            }
+            else
+            {
+                this.TintColor = Color.White;
+            }*/
+
+            base.Draw(spriteBatch);
+        }
+    }
+}
--- InsectGenerator/InsectGenerator/InsectGenerator/Game1.cs
+++ InsectGenerator/InsectGenerator/InsectGenerator/Game1.cs
@@ -17,10 +17,10 @@ namespace InsectGenerator
         SpriteBatch spriteBatch;
         Texture2D background, spritesheet;
         Random rand = new Random(System.Environment.TickCount);
-
-        List<Sprite> bugs = new List<Sprite>();
-
-
+        float timeRemaining = 0.0f;
+        float timeTotal = 0.3f;
+        float TimePerUpdate = 2.00f;
+        List<Bug> bugs = new List<Bug>();
         int bugNum = 100;
 
         public Game1()
@@ -42,6 +42,24 @@ namespace InsectGenerator
             base.Initialize();
         }
 
+        public void SpawnBug(Vector2 location)
+        {
+            int bugX = rand.Next(0, 3);
+            int bugY = rand.Next(0, 2);
+            int X = rand.Next(40, 150);
+            int Y = rand.Next(-40, 40);
+            if (Y == 0)
+            {
+                Y = 10;
+            }
+
+            Vector2 velocity = new Vector2(X, Y);
+            velocity.Normalize();
+            velocity *= 200;
+            
+            Bug bug = new Bug(location, spritesheet, new Rectangle(64 * bugX, 64 * bugY, 64, 64), velocity);
+            bugs.Add(bug);
+        }
 
         protected override void LoadContent()
         {
@@ -50,33 +68,24 @@ namespace InsectGenerator
             spritesheet = Content.Load<Texture2D>("spritesheet");
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            for (int i = 0; i < bugNum; i++)
+
+            for (int x = 0; x < 10; x++)
             {
-                int bugX = rand.Next(0, 3);
-                int bugY = rand.Next(0, 2);
-                Sprite bug = new Sprite(new Vector2 (rand.Next(-400,-50),rand.Next(50,400)), spritesheet, new Rectangle(64*bugX, 64*bugY, 64, 64), new Vector2(rand.Next(40,150),rand.Next(-40,40)));
-                bugs.Add(bug);
-            }
-        }
-
-        public void moveBugs()
-        {
-            for (int i = 0; i < bugs.Count; i++)
-            {
-
-
-
-                //todo:collision
-                for (int j = 0; j < bugs.Count; j++)
+                for (int y = 0; y < 10; y++)
                 {
-                    if (bugs[i].IsBoxColliding(bugs[j].BoundingBoxRect))
-                    {
-
-                    }
+                    SpawnBug(new Vector2(-50 - (x*100) + rand.Next(-40, 40), 10 + y * 45 + rand.Next(-10, 10)));
                 }
             }
 
+
+            for (int i = 0; i < bugNum; i++)
+            {
+                
+            }
+            
         }
+
+
 
         protected override void UnloadContent()
         {
@@ -90,22 +99,56 @@ namespace InsectGenerator
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
-            moveBugs();
-
+            
             for (int i = 0; i < bugs.Count; i++)
             {
                 bugs[i].Update(gameTime);
+                bugs[i].mood = BugMoods.Normal;
+                if (bugs[i].Location.X > this.Window.ClientBounds.Width && bugs[i].Velocity.X < 0) 
+                {
+                    bugs[i].Velocity *= new Vector2(-1, 1);
+                    //bugs[i].FlipHorizontal = true; 
+                }
+
+                if (bugs[i].Location.X > -70 && bugs[i].Velocity.X < 0)
+                {
+                    bugs[i].Velocity *= new Vector2(-1, 1);
+                    //bugs[i].FlipHorizontal = true; 
+                }
+
+                for (int j = 0; j < bugs.Count; j++)
+                {
+                   
+                    if (bugs[i].IsBoxColliding(bugs[j].BoundingBoxRect))
+                    {
+                        bugs[i].mood = BugMoods.Angry;
+                    }
+                }
             }
+
+
 
             base.Update(gameTime);
         }
 
-
+        public void Method(GameTime gameTime)
+        {
+        //
+        
+         timeRemaining = MathHelper.Max(0, timeRemaining -
+        (float)gameTime.ElapsedGameTime.TotalSeconds);
+        if (timeRemaining == 0.0f)
+         {
+            timeRemaining = TimePerUpdate;
+         }
+        }
+        
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
+            spriteBatch.Draw(background, new Rectangle(0, 0, this.Window.ClientBounds.Width, this.Window.ClientBounds.Height), Color.White); // Draw the background at (0,0) - no crazy tinting
 
 
             for (int i = 0; i < bugs.Count; i++)
